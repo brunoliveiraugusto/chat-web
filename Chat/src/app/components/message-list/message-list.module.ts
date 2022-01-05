95744cc6c50b306695673c5716f7d1a255ab11cb
= <=>
2022-01-05 16:16:32 -0300

    Criação de componente para listagem de mensagens.

---
+++ Chat/src/app/components/message-list/message-list.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+
+import { MessageListComponent } from './message-list.component';
+
+@NgModule({
+  declarations: [MessageListComponent],
+  exports: [MessageListComponent],
+  imports: []
+})
+export class MessageListModule {
+
+}
