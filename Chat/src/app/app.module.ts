b0067b36aaf323d7e853d66efd34ce04f66c3640
Bruno Augusto <brunoaugustoliveira@outlook.com>
2022-01-07 16:35:30 -0300

    Remoção de import automático.

--- Chat/src/app/app.module.ts
+++ Chat/src/app/app.module.ts
@@ -5,12 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatModule } from './components/chat/chat.module';
 import { UserModule } from './components/user/user.module';
-import { ValidationMessageComponent } from './shared/components/validation-message/validation-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ValidationMessageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
