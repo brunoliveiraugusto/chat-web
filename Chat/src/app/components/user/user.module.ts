1dfcfa728bbfa3b7425e2c879c279cfdaf449408
Bruno Augusto <brunoaugustoliveira@outlook.com>
2022-01-10 14:27:58 -0300

    Criação de componente para recuperação de senha.

--- Chat/src/app/components/user/user.module.ts
+++ Chat/src/app/components/user/user.module.ts
@@ -6,10 +6,11 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from './../../shared/shared.module';
 import { SignupComponent } from './sign-up/signup.component';
 import { SigninComponent } from './sign-in/signin.component';
+import { RecoverPasswordComponent } from './recover-password/recover-password.component';
 
 @NgModule({
-  declarations: [SigninComponent, SignupComponent],
-  exports: [SigninComponent, SignupComponent],
+  declarations: [SigninComponent, SignupComponent, RecoverPasswordComponent],
+  exports: [SigninComponent, SignupComponent, RecoverPasswordComponent],
   imports: [ReactiveFormsModule, RouterModule, SharedModule, CommonModule]
 })
 export class UserModule {
